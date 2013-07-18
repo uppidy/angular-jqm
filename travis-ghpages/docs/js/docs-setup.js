d4446f3f77401b2cfc6b328b70fb88398c75c2d2
Travis <travis@travis-ci.org>
2013-07-18 08:53:35 +0200

    chore(build): Travis build  pushed to gh-pages

---
+++ travis-ghpages/docs/js/docs-setup.js
@@ -0,0 +1,132 @@
+NG_DOCS={
+  "sections": {
+    "guide": "Guide",
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "guide",
+      "id": "index",
+      "shortName": "Guide",
+      "type": "overview",
+      "keywords": "check guide hello out overview page page1 the to welcome"
+    },
+    {
+      "section": "guide",
+      "id": "page1",
+      "shortName": "Page 1",
+      "type": "overview",
+      "keywords": "guide here is overview page"
+    },
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "API Documentation",
+      "type": "overview",
+      "keywords": "angular-jqm api check documentation examples get hello is live out overview started the this to with"
+    },
+    {
+      "section": "api",
+      "id": "jqm",
+      "shortName": "jqm",
+      "type": "overview",
+      "keywords": "all api code contains is jqm module one overview that the"
+    },
+    {
+      "section": "api",
+      "id": "jqm.$anchorScroll",
+      "shortName": "$anchorScroll",
+      "type": "function",
+      "keywords": "$anchorscroll $hideaddressbar address and angular api bar by calls change default every function hidden instead is jqm of on orientation overrides the this view"
+    },
+    {
+      "section": "api",
+      "id": "jqm.$hideAddressBar",
+      "shortName": "$hideAddressBar",
+      "type": "function",
+      "keywords": "$hideaddressbar $orientation $rootelement $window address api bar called devices function hide it jqm mobile on support that the this when will"
+    },
+    {
+      "section": "api",
+      "id": "jqm.$orientation",
+      "shortName": "$orientation",
+      "type": "function",
+      "keywords": "$orientation $orientationchanged $rootscope $window access also and api broadcast browser changes digest do event function jqm of on orientation provides root scope the this to whenever will"
+    },
+    {
+      "section": "api",
+      "id": "jqm.directive:jqmCheckbox",
+      "shortName": "jqmCheckbox",
+      "type": "directive",
+      "keywords": "angular anything api assignable be below can checkbox checky creates data-bind directive disable disabled disably element expression for given got guy html icon iconpos index inside is isdisabled jqm jqm-checkbox jquery label left me mini mobile module my name ng-false-value ng-model ng-true-value ngfalsevalue ngmodel ngtruevalue no not of on options or position right selected set should some tag the this to toggle true value ve when whether which will yes"
+    },
+    {
+      "section": "api",
+      "id": "jqm.directive:jqmFlip",
+      "shortName": "jqmFlip",
+      "type": "directive",
+      "keywords": "angular anything api assignable be creates data-bind directive disabled element expression flip fliped given html index inside is jqm jqm-flip jquery label mobile module my name ng-model ngmodel ngofflabel ngoffvalue ngonlabel ngonvalue off off-label off-value on on-label on-value set should shown switch tag the this to value when whether which will"
+    },
+    {
+      "section": "api",
+      "id": "jqm.directive:jqmFooter",
+      "shortName": "jqmFooter",
+      "type": "directive",
+      "keywords": "api class defines directive footer height hello html index jqm jqm-footer jqm-page jqm-standalone-page module name of page1 style the world"
+    },
+    {
+      "section": "api",
+      "id": "jqm.directive:jqmHeader",
+      "shortName": "jqmHeader",
+      "type": "directive",
+      "keywords": "api class defines directive header height hello html index jqm jqm-header jqm-page jqm-standalone-page module name of page1 style the world"
+    },
+    {
+      "section": "api",
+      "id": "jqm.directive:jqmOnceClass",
+      "shortName": "jqmOnceClass",
+      "type": "directive",
+      "keywords": "api class directive given html index jqm jqm-once-class module name ng-init ng-model no once sets someclass string text the type watching with"
+    },
+    {
+      "section": "api",
+      "id": "jqm.directive:jqmPage",
+      "shortName": "jqmPage",
+      "type": "directive",
+      "keywords": "adds also api automatic class content creates directive for height html index it jqm jqm-page jqm-standalone-page jquery line mobile module name overflow page scrolling style world"
+    },
+    {
+      "section": "api",
+      "id": "jqm.directive:jqmPositionAnchor",
+      "shortName": "jqmPositionAnchor",
+      "type": "directive",
+      "keywords": "$position added an and angular api are child directive element elements every for from has html in index jqm jqm-position-anchor keep module moved name ng-controller noop or own property removed scope set that the this updated value whenever will"
+    },
+    {
+      "section": "api",
+      "id": "jqm.directive:jqmScrollable",
+      "shortName": "jqmScrollable",
+      "type": "directive",
+      "keywords": "adds an api directive element height html index jqm jqm-scrollable line module name overflow scrolling style to world"
+    },
+    {
+      "section": "api",
+      "id": "jqm.directive:jqmTheme",
+      "shortName": "jqmTheme",
+      "type": "directive",
+      "keywords": "$theme adding and api by children directive directives element evaluate for html index it jqm jqm-checkbox jqm-theme jqmcheckbox like module name other property scope sets that the theme this to"
+    }
+  ],
+  "apis": {
+    "guide": false,
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "jquery.mobile.css.js",
+    "angular.min.js",
+    "angular-mobile.js",
+    "angular-jqm.js"
+  ]
+};
