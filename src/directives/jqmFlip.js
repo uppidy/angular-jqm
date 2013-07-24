2bade26e3f9e7e3c14e5094cb72c1f250fb76670
Norbert Schneider <Norbert-Schneider@gmx.de>
2013-07-24 21:14:42 +0200

    feat(jqmFlip): Add handling of boolean and default values
    The on and off values "true" and "false" are parsed and bound as boolean
    values. Furthermore if no on and off values are set on will be bound to
    "true" and off to "false".
    
    Closes #71, Closes #91

--- src/directives/jqmFlip.js
+++ src/directives/jqmFlip.js
@@ -8,19 +8,34 @@
  *
  * Anything inside the `jqm-flip` tag will be a label.
  *
+ * Labels for the on and off state can be omitted.
+ * If no values for the on and off state are specified on will be bound to true and off to false.
+ *
+ * A theme can be set with the jqm-theme directive and specific styles can be set with the ng-style parameter.
+ * This is necessary to extend the width of the flip for long labels.
+ *
  * @param {expression=} ngModel Assignable angular expression to data-bind to.
  * @param {string=} disabled Whether this flip switch is disabled.
- * @param {string=} ngOnLabel The label which should be shown when fliped on.
- * @param {string=} ngOnValue The value to which the expression should be set when fliped on.
- * @param {string=} ngOffLabel The label which should be shown when fliped off.
- * @param {string=} ngOffValue The value to which the expression should be set when fliped off.
+ * @param {string=} mini Whether this flip should be displayed minified.
+ * @param {string=} ngOnLabel The label which should be shown when fliped on (optional).
+ * @param {string=} ngOnValue The value to which the expression should be set when fliped on (optional, default: true).
+ * @param {string=} ngOffLabel The label which should be shown when fliped off (optional).
+ * @param {string=} ngOffValue The value to which the expression should be set when fliped off (optional, default:false).
  *
  * @example
 <example module="jqm">
   <file name="index.html">
-    <div jqm-flip ng-model="flip" on-label="On" on-value="1" off-label="Off" off-value="0">
-      My value is: {{flip}}
-    </div>
+   <p>Selected value is: {{flip}}</p>
+   <div jqm-flip ng-model="flip">
+     Default values true/false
+   </div>
+   <div jqm-flip ng-model="flip" jqm-theme="e">
+     With theme
+   </div>
+   <div jqm-flip ng-model="flip2" on-label="On" on-value="On" off-label="Off" off-value="Off">
+     My value is {{flip2}}
+   </div>
+   <div jqm-flip ng-model="flip3" on-label="Long On Label" off-label="Long Off Label" flipStyle="width: 11em;"/>
   </file>
 </example>
  */
@@ -36,45 +51,70 @@ jqmModule.directive('jqmFlip', [function () {
             offLabel: '@',
             offValue: '@',
             mini: '@',
-            disabled: '@'
+            disabled: '@',
+            flipStyle: '@'
         },
         require: ['?ngModel', '^?jqmControlgroup'],
         link: function (scope, element, attr, ctrls) {
             var ngModelCtrl = ctrls[0];
             var jqmControlGroupCtrl = ctrls[1];
+            var parsedOn;
+            var parsedOff;
 
             scope.theme = scope.$theme || 'c';
             scope.isMini = isMini;
+            scope.onValue = angular.isDefined(attr.onValue) ? scope.onValue : true;
+            scope.offValue = angular.isDefined(attr.offValue) ? scope.offValue : false;
 
             initToggleState();
             bindClick();
 
             function initToggleState () {
+                ngModelCtrl.$parsers.push(parseBoolean);
+                parsedOn = parseBoolean(scope.onValue);
+                parsedOff = parseBoolean(scope.offValue);
                 ngModelCtrl.$render = updateToggleStyle;
                 ngModelCtrl.$viewChangeListeners.push(updateToggleStyle);
             }
 
             function updateToggleStyle () {
+                updateNaNAsOffValue();
                 var toggled = isToggled();
                 scope.toggleLabel = toggled ? scope.onLabel : scope.offLabel;
                 scope.onStyle = toggled ? 100 : 0;
                 scope.offStyle = toggled ? 0 : 100;
             }
 
+            // this has to be done in the change listener,
+            // otherwise the potential scope value would be overwritten with the off value
+            function updateNaNAsOffValue () {
+                if (!ngModelCtrl.$viewValue) {
+                    ngModelCtrl.$setViewValue(parsedOff);
+                }
+            }
+
             function bindClick () {
                 scope.toggle = function () {
-                    ngModelCtrl.$setViewValue(isToggled() ? scope.offValue : scope.onValue);
+                    ngModelCtrl.$setViewValue(isToggled() ? parsedOff : parsedOn);
                 };
             }
 
             function isToggled () {
-                return ngModelCtrl.$viewValue === scope.onValue;
+                return ngModelCtrl.$viewValue === parsedOn;
             }
 
             function isMini() {
                 return scope.mini || (jqmControlGroupCtrl && jqmControlGroupCtrl.$scope.mini);
             }
 
+            function parseBoolean(value) {
+                if (value === 'true') {
+                    return true;
+                } else if (value === 'false') {
+                    return false;
+                }
+                return value;
+            }
         }
     };
 }]);
