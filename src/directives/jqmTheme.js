d1fc5cd1c90eee4f6f8eea41a6fb39ce2d0bc433
Andy Joslin <andytjoslin@gmail.com>
2013-11-30 19:52:50 -0500

    docs(jqmTheme): update docs

--- src/directives/jqmTheme.js
+++ src/directives/jqmTheme.js
@@ -7,6 +7,14 @@
  * Sets the jqm theme for this element and it's children by adding a `$theme` property to the scope.
  * Other directives like `jqmCheckbox` evaluate that property.
  *
+ * jqmTheme will modify the scope of whichever element you give it, adding `$theme`.  
+ * That means if you apply jqmTheme to an element that shares its scope with its parent, 
+ * it will set the theme for that element and its parent.  
+ * If you explicitly want jqmTheme to set a theme for only itself and its children, 
+ * share it with a directive that creates its own scope. 
+ *
+ * (think this is bad?  come discuss it at [the issue](https://github.com/angular-widgets/angular-jqm/issues/177))
+ *
  * @example
  <example module="jqm">
  <file name="index.html">
